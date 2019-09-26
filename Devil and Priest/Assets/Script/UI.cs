5684897103c05e03ee0b19b6e4c5cd7b8d60f7ec
hsyjkjkl <w240629738@163.com>
2019-09-26 08:49:04 +0800

    Update

--- Devil and Priest/Assets/Script/UI.cs
+++ Devil and Priest/Assets/Script/UI.cs
@@ -12,21 +12,23 @@ public class UI : MonoBehaviour {
 		interaction = Director.getInstance ().currentSceneController as Interaction;
 
 		style1 = new GUIStyle();
-		style1.fontSize = 40;
+		style1.fontSize = 35;
 		style1.alignment = TextAnchor.MiddleCenter;
 
 		style2 = new GUIStyle("button");
-		style2.fontSize = 30;
+		style2.fontSize = 25;
 	}
     void OnGUI() {
 		if (status == 0) {
-			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "Gameover!", style1);
-			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 140, 70), "Restart", style2)) {
+			style1.normal.textColor = Color.red;
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-95, 100, 50), "Gameover!", style1);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 150, 70), "Play again!", style2)) {
 				status = 1;
 				interaction.Restart ();
 			}
 		} else if(status == 2) {
-			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "You win!", style1);
+			style1.normal.textColor = Color.black;
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-95, 100, 50), "You win!", style1);
 			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 140, 70), "Restart", style2)) {
 				status = 1;
 				interaction.Restart ();
